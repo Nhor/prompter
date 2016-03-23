f8ac2154f3c1867e229472c6842dd6dab34642e0
Norbert Mieczkowski <mieczkowskinorbert@gmail.com>
2016-03-23 22:04:08 +0100

    Created dynamic language setting function and made US English Female a default langauge

--- Prompter/PrompterWeb/AddIn/Prompter/Prompter.js
+++ Prompter/PrompterWeb/AddIn/Prompter/Prompter.js
@@ -12,13 +12,29 @@
         .sortBy()
         .value();
 
+    var setLanguage = function (language) {
+        responsiveVoice.setDefaultVoice(language);
+    };
+
     Office.initialize = function (reason) {
         $(document).ready(function () {
             app.initialize();
 
-            _.forEach(responsiveVoiceLanguages, function (language) {
-                $('.language-selection').append('<option>' + language + '</option>');
+            _.forEach(responsiveVoiceLanguages, function (language, index) {
+                $('.language-selection').append(
+                    '<option value="' + index + '">' + language + '</option>'
+                );
             });
+
+            $('.language-selection').change(function () {
+                setLanguage($('.language-selection option:selected').text());
+            });
+
+            $('.language-selection').val(
+                _.findIndex(responsiveVoiceLanguages, function (language) {
+                    return language === 'US English Female';
+                })
+            );
         });
     };
 })();
