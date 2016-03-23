a3d720ca5ae4709223670cc0d08234bb0d7e2175
Norbert Mieczkowski <mieczkowskinorbert@gmail.com>
2016-03-23 20:54:44 +0100

    Added lodash to the project and created dummy language selection

--- Prompter/PrompterWeb/AddIn/Prompter/Prompter.js
+++ Prompter/PrompterWeb/AddIn/Prompter/Prompter.js
@@ -1,9 +1,24 @@
 ﻿(function () {
     'use strict';
 
+    var responsiveVoiceLanguages = _
+        .chain(responsiveVoice.getVoices())
+        .map(function (obj) {
+            return obj.name;
+        })
+        .filter(function (language) {
+            return !_.startsWith(language, 'Fallback');
+        })
+        .sortBy()
+        .value();
+
     Office.initialize = function (reason) {
         $(document).ready(function () {
             app.initialize();
+
+            _.forEach(responsiveVoiceLanguages, function (language) {
+                $('.language-selection').append('<option>' + language + '</option>');
+            });
         });
     };
 })();
