8a6d366a1803ac5716a21c26aae0a4c8e523d488
Norbert Mieczkowski <mieczkowskinorbert@gmail.com>
2016-03-24 22:59:55 +0100

    Added when to the project and created SlideManager module

---
+++ Prompter/PrompterWeb/AddIn/libs/SlideManager.js
@@ -0,0 +1,30 @@
+﻿var SlideManager = (function () {
+    'use strict';
+
+    var SlideManager = function () {
+    };
+
+    SlideManager.prototype.getMode = function () {
+        return when.promise(function (resolve, reject) {
+            Office.context.document.getActiveViewAsync(function (res) {
+                if (res.status === 'failed') {
+                    return reject(new Error(res.error.message));
+                }
+                return resolve(res.value);
+            });
+        });
+    };
+
+    SlideManager.prototype.getCurrent = function () {
+        return when.promise(function (resolve, reject) {
+            Office.context.document.getSelectedDataAsync(Office.CoercionType.SlideRange, function (res) {
+                if (res.status === 'failed') {
+                    return reject(new Error(res.error.message));
+                }
+                return resolve(_.first(res.value.slides));
+            });
+        });
+    };
+
+    return SlideManager;
+})();
