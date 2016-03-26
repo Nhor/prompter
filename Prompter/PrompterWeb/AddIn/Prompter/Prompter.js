3a6cbff2b5187c11c888508435bbd0d06f5feb32
Norbert Mieczkowski <mieczkowskinorbert@gmail.com>
2016-03-26 09:31:17 +0100

    Grouped the initialization functions and added docstrings to each

--- Prompter/PrompterWeb/AddIn/Prompter/Prompter.js
+++ Prompter/PrompterWeb/AddIn/Prompter/Prompter.js
@@ -1,32 +1,49 @@
 ﻿(function () {
     'use strict';
 
+    var textToSpeech = new TextToSpeech();
+    var slideManager = new SlideManager();
+
+    /**
+     * Initialize the .language-selection <select> items to display available
+     * languages and change the TTS language each time the <option> changes.
+     */
+    var initializeLanguageSelection = function () {
+        _.forEach(textToSpeech.languages, function (language, index) {
+            $('.language-selection').append(
+                '<option value="' + index + '">' + language + '</option>'
+            );
+        });
+
+        $('.language-selection').change(function () {
+            textToSpeech.setLanguage($('.language-selection option:selected').text());
+        });
+
+        $('.language-selection').val(
+            _.findIndex(textToSpeech.languages, function (language) {
+                return language === textToSpeech.languageFallback;
+            })
+        );
+    };
+
+    /**
+     * Initialize the #on-off-switch <input> item to ``SlideManager.watch`` or
+     * ``SlideManager.stopWatching`` the slideshow according to the checkbox state.
+     */
+    var initializeOnOffSwitch = function () {
+        $('#on-off-switch:checkbox').change(function () {
+            if (!this.checked) {
+                return slideManager.stopWatching();
+            }
+            return slideManager.watch();
+        });
+    };
+
     Office.initialize = function (reason) {
         $(document).ready(function () {
             app.initialize();
-
-            var textToSpeech = new TextToSpeech();
-            _.forEach(textToSpeech.languages, function (language, index) {
-                $('.language-selection').append(
-                    '<option value="' + index + '">' + language + '</option>'
-                );
-            });
-            $('.language-selection').change(function () {
-                textToSpeech.setLanguage($('.language-selection option:selected').text());
-            });
-            $('.language-selection').val(
-                _.findIndex(textToSpeech.languages, function (language) {
-                    return language === textToSpeech.languageFallback;
-                })
-            );
-
-            var slideManager = new SlideManager();
-            $('#on-off-switch:checkbox').change(function () {
-                if (!this.checked) {
-                    return slideManager.stopWatching();
-                }
-                return slideManager.watch();
-            });
+            initializeLanguageSelection();
+            initializeOnOffSwitch();
         });
     };
 })();
